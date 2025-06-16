567f360dd37a7fd31b0c9b8fd881a3f6518a1238
ragOP <najmiraghib@gmail.com>
2025-06-16 22:23:52 +0530

    update Google Tag Manager integration and replace references to burialprotectionbenefit.org with finalcoveragebenefit.org

--- cong.php
+++ cong.php
@@ -5,10 +5,20 @@
   <title>Congratulations</title>
   <link rel="stylesheet" href="stylepage.css">
 <script src="https://b-js.ringba.com/CAfdb7eb5bdaa24fb8a155f293ece397ae" async></script>
-
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KS2BR6B7');</script>
+<!-- End Google Tag Manager -->
 
 </head>
 <body>
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KS2BR6B7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
   <div class="checking" id="congratulations">
     <div class="congrats">Congratulations, You Qualify 🎉 </div>
     <div class="top-description-5">
